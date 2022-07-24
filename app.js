ccb26ee2bc3ecded2bbac2dbffd1a9f21235d4f9
Pru <lms314044s2@gmail.com>
2022-07-24 20:12:55 +0900

    Add crystal calc & delete Memo spellcheck

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const menuBtn = menu.querySelector("button");
 const menuCounter = menu.querySelector("span:last-child");
 const saveMemoBtn = document.getElementById("save-memo--btn");
 const memoArea = document.querySelector(".memo--textarea");
+const crystal_container = document.getElementById("crystal_container");
+const crystal1 = crystal_container.querySelector("#crystal1");
+const crystal2 = crystal_container.querySelector("#crystal2");
 
 let allValue = 0;
 let maxCount = 0;
@@ -54,6 +57,7 @@ function createForm() {
 }
 createForm();
 
+//메모
 memoArea.addEventListener("change", () => {
   localStorage.setItem("memo", memoArea.value);
 });
@@ -61,12 +65,35 @@ saveMemoBtn.addEventListener("click", () => {
   localStorage.setItem("memo", memoArea.value);
 });
 
-itemList.addEventListener("change", () => {
-  const a = itemList.querySelectorAll("form span");
+const addItemValue = () => {
+  const itemValueList = itemList.querySelectorAll("form span");
+  const crystalValue = crystal_container.querySelectorAll(
+    "div span:last-child"
+  );
+
   allValue = 0;
-  a.forEach((item) => {
+  itemValueList.forEach((item) => {
+    allValue = allValue + parseInt(item.innerText);
+  });
+  crystalValue.forEach((item) => {
     allValue = allValue + parseInt(item.innerText);
   });
   menuCounter.innerText = `${allValue} 길`;
-});
+};
+
+crystal_container.addEventListener("change", addItemValue);
+itemList.addEventListener("change", addItemValue);
 menuBtn.addEventListener("click", createForm);
+
+//크리스탈 계산함수
+const crystal_value = (event) => {
+  const path = event.composedPath();
+  const crystal_count = path[1].querySelector(".item-input__crystal--count");
+  const crystal_price = path[1].querySelector(".item-input__crystal--price");
+  const crystal_value = path[1].querySelector("span:last-child");
+  crystal_value.innerHTML = crystal_count.value * crystal_price.value;
+  console.log();
+};
+
+crystal1.addEventListener("change", crystal_value);
+crystal2.addEventListener("change", crystal_value);
